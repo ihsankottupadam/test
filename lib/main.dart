c51077f9c2e6e225613ce939f4f06a1fb29935c4
Ihsan Kottupadam <ihsankottupdam@gmail.com>
2022-06-21 15:56:25 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "TestApp",
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
